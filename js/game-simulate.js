cad74e52151516980e2b2dde4e149cb3a729bac8
Gautier Maire <gautier.maire@dotsafe.fr>
2020-09-23 16:08:14 +0200

    Add timer

--- js/game-simulate.js
+++ js/game-simulate.js
@@ -36,11 +36,37 @@ function setSpecScore() {
 }
 
 // Set timer
+/*
+ * Basic Count Up from Date and Time
+ * Author: @mrwigster / https://guwii.com/bytes/count-date-time-javascript/
+ */
+function setSpecTimer(countFrom, id) {
+  countFrom = new Date(countFrom).getTime();
+  var now = new Date(),
+      countFrom = new Date(countFrom),
+      timeDifference = (now - countFrom);
 
+  var secondsInADay = 60 * 60 * 1000 * 24,
+      secondsInAHour = 60 * 60 * 1000;
+
+  days = Math.floor(timeDifference / (secondsInADay) * 1);
+  hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+  mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+  secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+  var idEl = document.getElementById(id);
+  idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+  idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+
+  clearTimeout(setSpecTimer.interval);
+  setSpecTimer.interval = setTimeout(function(){ setSpecTimer(countFrom, id); }, 1000);
+}
 
 // Initialise
 setSpecGameOnOff();
+setSpecGameName();
 setSpecPlayersNames();
+setSpecTimer("Sep 23, 2020 15:55:00", 'countup1');
 
 
 function getData(){
